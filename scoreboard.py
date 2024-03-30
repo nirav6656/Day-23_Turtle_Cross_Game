111a121fa54af17e0879ec6bf89eab6e426e7305
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-03-30 12:26:31 -0400

    add scoreboad and increase the spee of turtle everytime it go to next level

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,18 @@
+from turtle import Turtle,Screen
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.goto(-230,230)
+        self.score = 0
+        self.write(self.score, align="left", font=FONT)
+
+
+    def update_score(self):
+        self.clear()
+        self.score +=1
+        self.write(self.score, align="left", font=FONT)
+
